228e32b4f3a2aba5e577c84108c60448d4f38a2e
omarkharimov-ui <omarkharimov@gmail.com>
2026-05-27 19:13:46 +0400

    chore: remove Capacitor, android/, sw.js and PWA install artifacts

--- android/app/src/main/java/com/saito/admin/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.saito.admin;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.saito.admin',
-  appName: 'Saito Admin',
-  webDir: 'public'
-};
-
-export default config;

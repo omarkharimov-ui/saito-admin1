ae2225de13f2bc7e2c70b1d981c6a1f93ede338f
omarkharimov-ui <omarkharimov@gmail.com>
2026-05-27 19:25:20 +0400

    fix: add self-destruct sw.js to kill cached service worker on client browsers

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+// Self-destruct: unregister this service worker and clear all caches
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => Promise.all(names.map((n) => caches.delete(n))))
+      .then(() => self.registration.unregister())
+  );
+});

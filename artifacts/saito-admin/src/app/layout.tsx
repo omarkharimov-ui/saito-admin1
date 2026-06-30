11c66f21a0b7faa23222481170cb8e19e62a75ef
omarkharimov-ui <omarkharimov@gmail.com>
2026-06-30 17:02:24 +0400

    fix: restore UIProvider and CartProvider in layout

--- artifacts/saito-admin/src/app/layout.tsx
+++ artifacts/saito-admin/src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import { LanguageProvider } from "@/lib/i18n/LanguageContext";
 import { LanguageProvider as LegacyLanguageProvider } from "@/context/LanguageContext";
+import { UIProvider } from "@/context/UIContext";
+import { CartProvider } from "@/context/CartContext";
 import ClientLayout from "@/components/layout/ClientLayout";
 import { CustomNotificationProvider } from "@/components/CustomNotification";
 import ServiceWorkerRegistration from "@/components/ServiceWorkerRegistration";
@@ -66,12 +68,16 @@ export default function RootLayout({
         <PWAInstallPrompt />
         <LanguageProvider>
           <LegacyLanguageProvider>
-            <CustomNotificationProvider />
-            <ClientLayout>
-              <main className="flex-1 overflow-y-auto scrollbar-none">
-                {children}
-              </main>
-            </ClientLayout>
+            <UIProvider>
+              <CartProvider>
+                <CustomNotificationProvider />
+                <ClientLayout>
+                  <main className="flex-1 overflow-y-auto scrollbar-none">
+                    {children}
+                  </main>
+                </ClientLayout>
+              </CartProvider>
+            </UIProvider>
           </LegacyLanguageProvider>
         </LanguageProvider>
       </body>
